955af377dcd4518aee028bf5b14034cbb794bb62
Sananda Som <Sean@Sanandas-MacBook-Pro.local>
2018-07-11 20:33:14 -0500

    fixed error

--- models/Article.js
+++ models/Article.js
@@ -1,17 +1,16 @@
-// Require mongoose
+
 var mongoose = require("mongoose");
-// Create Schema class
+
 var Schema = mongoose.Schema;
 
 // Create article schema
 var ArticleSchema = new Schema({
-  // title is a required string
+
   title: {
     type: String,
     required: true
   },
-  // link is a required string
-  // todo save summary paragraph instead of link
+ 
   link: {
     type: String,
     required: true
@@ -26,5 +25,5 @@ var ArticleSchema = new Schema({
 // Create the Article model with the ArticleSchema
 var Article = mongoose.model("Article", ArticleSchema);
 
-// Export the model
+
 module.exports = Article;
